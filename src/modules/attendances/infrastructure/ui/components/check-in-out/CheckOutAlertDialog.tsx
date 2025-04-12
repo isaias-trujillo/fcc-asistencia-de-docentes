03590f1cc1f6cde9779a1d324f21ec10280eb000
Isaias Trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2025-04-12 12:10:18 -0500

    make text bigger and rearrange elements

--- src/modules/attendances/infrastructure/ui/components/check-in-out/CheckOutAlertDialog.tsx
+++ src/modules/attendances/infrastructure/ui/components/check-in-out/CheckOutAlertDialog.tsx
@@ -15,14 +15,14 @@ import {SendIcon} from "lucide-react";
 const CheckOutAlertDialog = ({onSubmit} : { onSubmit: () => void }) => (
     <AlertDialog>
         <AlertDialogTrigger asChild>
-            <Button className={"max-sm:grow"}>
-                <SendIcon /> Marcar mi salida
+            <Button className={"max-sm:grow p-[clamp(1rem,1.75rem+1vh,2rem)] text-[clamp(0.75rem,0.75rem+1dvw,1.25rem)]"}>
+                <SendIcon className='size-[clamp(1rem,1.25rem+1dvw,1.5rem)]'/> Marcar mi salida
             </Button>
         </AlertDialogTrigger>
         <AlertDialogContent>
             <AlertDialogHeader>
-                <AlertDialogTitle>¿Estás seguro de marcar tu salida?</AlertDialogTitle>
-                <AlertDialogDescription className={'font-semibold text-accent-foreground'}>
+                <AlertDialogTitle className={"text-[clamp(0.75rem,1rem+1dvw,1.5rem)] text-left"}>¿Estás seguro de marcar tu salida?</AlertDialogTitle>
+                <AlertDialogDescription className={'text-accent-foreground text-[clamp(0.75rem,0.75rem+1dvw,1rem)] text-left'}>
                     Si tienes más horas pendientes, tendrás que volver a marcar tu entrada y pasar lista a tus alumnos de nuevo.
                 </AlertDialogDescription>
             </AlertDialogHeader>
